91b57eb6be97cc0643cdf9188f2a8b5e862ac469
Vashudev <vmanishkumar04@gmail.com>
2023-09-25 00:55:21 +0530

    new changes

--- ui/src/components/Authentication/Signup.js
+++ ui/src/components/Authentication/Signup.js
@@ -101,7 +101,7 @@ const Signup = () => {
       const data = new FormData();
       data.append("file", pics);
       data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
+      data.append("cloud_name", "manish(vashu)");
       fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
         method: "post",
         body: data,
